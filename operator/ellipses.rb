bc81a51e47c019d37273462f69276e03cf22bceb
Eric Bouchut <ebouchut@gmail.com>
2011-09-07 13:33:47 +0200

    Added operator and array basics

---
+++ operator/ellipses.rb
@@ -0,0 +1,15 @@
+#~~~~~~~~~~~~~~~~~~
+# Ellipses operator
+#~~~~~~~~~~~~~~~~~~
+puts "The double dot operator (1..3) iterates from 1 up to 3 (inclusive)"
+for i in 1..3
+	print "[#{i}] "
+end
+puts
+puts "The triple dot operator (1...3) iterates from 1 up to but not including 3"
+for i in 1...3
+	print "[#{i}] "
+end
+
+
+
