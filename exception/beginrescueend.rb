2b2e782e42599e2c1bbaee6e4247273a2c75114b
Eric Bouchut <ebouchut@gmail.com>
2012-03-13 21:35:30 +0100

    Added an example: throwing and catching an exception

---
+++ exception/beginrescueend.rb
@@ -0,0 +1,10 @@
+begin
+  fail "Unable to continue"
+  # raise and fail can be used interchangeably
+
+  puts "Should never reach this point"
+rescue => error # catches StandardError hierarchy
+  puts error.inspect
+ensure
+  puts "Cleans things up whether an exception occured or not"
+end
