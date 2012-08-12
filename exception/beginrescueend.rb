a035423cfe8b574664a04a86c8778a1be529e7e4
Eric Bouchut <ebouchut@gmail.com>
2012-08-12 20:43:51 +0200

    Update begin/rescue/end memo

--- exception/beginrescueend.rb
+++ exception/beginrescueend.rb
@@ -2,12 +2,19 @@
 #
 
 begin
-  fail "Unable to continue"
+  fail "Unable to continue"   # Raise a RuntimeException
   # raise and fail can be used interchangeably
 
   puts "Should never reach this point"
 rescue => error # catches StandardError hierarchy
   puts error.inspect
+  # Using retry here would go back to begin and replay it again
+  # Using raise here would reraise the current exception
+  # raise XyzException, "error message", Kernel.caller
+  #     create a XyzException with the passed in message and stack trace,
+  #     with the current method removed, then raise it.
+else
+  puts "No exception"
 ensure
   puts "Cleans things up whether an exception occured or not"
 end
